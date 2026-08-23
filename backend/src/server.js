51ac06297021cee2b8ba2cc675300dd85d4ace1c
kwasnakiwi <jakzab199@gmail.com>
2026-08-23 23:00:31 +0200

    creating server

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,6 +16,8 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("API działa na http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`API działa na porcie ${PORT}`);
 });
